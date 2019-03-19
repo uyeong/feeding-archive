9cd153be0a7674e6ccefb2d12fda399498bf9ab7
U-Yeong Ju <uyeong21c@gmail.com>
2019-03-19 11:50:45 +0900

    Change directory structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './Home';
+import Main from './Main';
+import './style.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/main/">Main</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/" exact component={Home} />
+      <Route path="/main/" component={Main} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
