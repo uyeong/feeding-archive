506b02dd1bc6c445c02c008638e9d4d43b5c56f4
U-Yeong Ju <uyeong21c@gmail.com>
2019-03-22 11:02:59 +0900

    Update config.sample.js

--- src/config.sample.js
+++ src/config.sample.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: '<API_KEY>',
+  authDomain: '<PROJECT_ID>.firebaseapp.com',
+  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
+  projectId: '<PROJECT_ID>',
+  storageBucket: '<BUCKET>.appspot.com',
+  messagingSenderId: '<SENDER_ID>',
 }
